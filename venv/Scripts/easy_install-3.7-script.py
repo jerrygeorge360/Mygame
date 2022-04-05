f5fdfeb4d8bd1eefbe76411b338ec2d537779eba
jerry george <jerrygithub360@gmail.com>
2022-04-05 09:13:43 -0700

    update

--- venv/Scripts/easy_install-3.7-script.py
+++ venv/Scripts/easy_install-3.7-script.py
@@ -1,4 +1,4 @@
-#!"C:\Users\EDEANI JERRY GEORGE\OneDrive\Desktop\digital dreams\mustpygame\venv\Scripts\python.exe"
+#!"C:\Users\EDEANI JERRY GEORGE\OneDrive\Desktop\digital dreams\Mygame\venv\Scripts\python.exe"
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.7'
 __requires__ = 'setuptools==40.8.0'
 import re
